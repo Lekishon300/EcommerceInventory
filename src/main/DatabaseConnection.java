bd9e3c8812a3240f02e1a06ef576347632e6e622
Lekishon300 <117873160+Lekishon300@users.noreply.github.com>
2024-03-03 12:31:26 -0600

    Comment DatabaseConnection.java

--- src/main/DatabaseConnection.java
+++ src/main/DatabaseConnection.java
@@ -5,9 +5,24 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.io.*;
 
+/**
+ * The DatabaseConnection class provides a mechanism to establish a connection to the database.
+ * It utilizes a properties file to configure the database connection parameters including the URL, username,
+ * and password. This class centralizes the database connection setup for reuse across different parts of the application
+ * that require database access.
+ */
 public class DatabaseConnection {
   private static Properties properties = new Properties();
-
+     /**
+     * Attempts to establish a connection to the database using parameters defined in a properties file.
+     * It first attempts to load the database connection details from "database.properties" file.
+     * After successfully loading the properties, it tries to establish a connection to the database
+     * using the JDBC URL, username, and password provided in the properties file.
+     * 
+     * @return A Connection object to the database if the connection is successfully established.
+     * @throws Exception if there is an issue loading the properties file, 
+     * class not found exception for the JDBC driver, or if there is a problem establishing the connection to the database.
+     */
   public static Connection getConnection()throws Exception{
     try{
       FileReader reader= new FileReader("database.properties");
